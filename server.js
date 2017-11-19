3f3a36a82100a0c8b235e32cb24f9323dee1a207
mrinal mitra <mmitra@aligntech.com>
2017-11-19 19:15:10 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs  = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+    });
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+    });
+
+//Middle ware starts    
+    
+app.use(express.static(__dirname+ '/public'));
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var logInfo = `${now} : ${req.method} ${req.path}` ;
+    fs.appendFile('server.log',logInfo + '\n', (err)=>{
+        if(err){
+            console.log('Unable to append to Server.log.')
+        }
+    });
+    console.log(logInfo);
+    next();
+});
+
+// app.use((req,res,next)=>{
+//     res.render('maintainence.hbs');
+// });
+
+
+
+//Middleware Ends
+
+app.set('view engine','hbs');
+app.get('/',(req,res)=>{
+  res.render('home.hbs',{
+    pageTitle:'Home Page Dynamic',
+    
+    welcomeMessage: 'Welcome to Home Page'
+  });
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About Page Dynamic'
+    });
+});
+
+app.listen(3000,()=>{
+   console.log('Server is up on the port 3000')
+});
